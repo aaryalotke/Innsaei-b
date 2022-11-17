4ba5981e9500ab4fd672cea35c4d70b1ee59c824
yashchavan10 <97660991+yashchavan10@users.noreply.github.com>
2022-11-17 21:23:10 +0530

    -17/11/22(2)

--- innsaeibackend/innsaeibackend/settings.py
+++ innsaeibackend/innsaeibackend/settings.py
@@ -47,15 +47,15 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        
-        
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
+
    ),
 }
 
