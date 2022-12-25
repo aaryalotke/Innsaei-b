739aa1250982958b6d9c42fc22cbbadf6bda2de1
yashchavan10 <97660991+yashchavan10@users.noreply.github.com>
2022-12-25 12:28:27 +0530

    update1 25/12

--- innsaeibackend/backend/urls.py
+++ innsaeibackend/backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/updateprofile/',views.updateUserProfile),
     path('users/verifyotp/', views.verifyOtp),
     path('users/logout/', views.userLogout),
-    path('users/gallery/',views.getEvent),
+    #path('users/gallery/',views.getEvent),
     path('users/ContactUs/',views.api_create_contact_view), #3
     path('users/editorial/',views.editorialsList), #4
     path('users/Developers/',views.developersList), #5
